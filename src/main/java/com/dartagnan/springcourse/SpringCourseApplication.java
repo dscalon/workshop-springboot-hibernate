c9a5e6a49d7fba98e10633b203495a4943c5fbc0
dscalon <dscalon@live.com>
2020-06-01 04:21:40 -0300

    User entity and resource created

--- src/main/java/com/dartagnan/springcourse/SpringCourseApplication.java
+++ src/main/java/com/dartagnan/springcourse/SpringCourseApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringcourseApplication {
+public class SpringCourseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringcourseApplication.class, args);
+        SpringApplication.run(SpringCourseApplication.class, args);
     }
 
 }
